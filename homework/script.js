052a73914de8a5d4a75c7be26bdb814ca04d82a8
antolow <98319496+antolow@users.noreply.github.com>
2022-10-14 13:16:55 +0500

    dz

---
+++ homework/script.js
@@ -0,0 +1,62 @@
+//2
+let str1 = 1;
+if (str1 > 0) {
+        console.log('Большше нуля')
+     } else {
+        console.log('Меньше нуля')
+     }
+
+//1
+let str =1;
+if (str == 1) {
+        console.log('Да')
+    } else {
+        console.log('Нет')
+    }
+
+//4
+    let str2 = prompt();
+let str3 = prompt();
+let str4 = prompt();
+if (str2 == 1) {
+    console.log(1)
+} else if (str3 == 2) {
+  console.log(2)
+} else if (str4 == 3) {
+  console.log (3)
+} else {
+    console.log ('Неизвестное значение')
+} 
+ 
+//3
+let n;
+let m = 13;
+if (m > 50) {
+    n = 'Большое'
+} else {
+    n = 'Маленькое'
+}
+
+//5
+let i = 44;
+while (i++ < 67) {
+    console.log(`$(i)`)
+}
+
+//6
+let = 44;
+while (i++ < 670) {
+    if (i % 10 == 0) {
+        console.log(`${i}`)
+    }
+}
+
+//7
+for (let i = 45; i <=67; i++) {
+    console.log(i)
+}
+
+for (let i = 45; i <= 670; i++) {
+    if (i % 10 == 0)
+    console.log(i)
+} 
