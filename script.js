2fe980cc376d9363b3356d8cb8065f7b6b8ab02e
antolow <98319496+antolow@users.noreply.github.com>
2022-10-26 21:05:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+function grade(a) {
+    if (a >=90) {
+        console.log('A')
+    }
+    if (a >=80) {
+        console.log('B')
+    }
+    if (a >=70) {
+        console.log('C')
+    }
+    if (a >=60) {
+        console.log('D')
+    }
+    else {
+        console.log('F')
+    }
+}
+
+grade(65)
+
+
+function check(a) {
+        console.log(a.toLowerCase(a))
+}
+
+check(DFLK)
